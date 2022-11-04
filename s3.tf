f842cf41c46ede8325b4a5d1f5063cfd5ea25219
Simon Hanmer <Simon.Hanmer@ecs.co.uk>
2022-11-04 15:22:11 +0000

    add force_destroy to s3 resource so we can destroy even with objects in bucket

--- s3.tf
+++ s3.tf
@@ -1,3 +1,4 @@
 resource "aws_s3_bucket" "test_bucket" {
-  bucket = "${var.project_name}-${data.aws_caller_identity.current.account_id}-test-bucket"
+  bucket        = "${var.project_name}-${data.aws_caller_identity.current.account_id}-test-bucket"
+  force_destroy = true
 }
